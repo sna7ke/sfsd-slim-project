88ed9bdd835c522deca5e0b14c07525356f5b0d9
Moataz <moatazaitmeziane20@gmail.com>
2024-12-10 09:17:04 +0000

    added tracking fields for student's state and block linking , and made according changes to functions
    i added a new field in the students struct which would track the student's state (wether it's deleted or not) , as well as a new field in the block struct which would point to the next block in the file (if it's contiguous it should be -1)

--- ms.c
+++ ms.c
@@ -23,6 +23,7 @@ void InitializeDisk(FILE *ms ,Disk D){
         //since the array of students varies on the blocking factor, we need to write the student's data directly
         fwrite(buffer.student,sizeof(Student),D.bf,ms);
         fwrite(&buffer.num,sizeof(int),1,ms);
+        fwrite(&buffer.next,sizeof(int),1,ms);
 
     }
 
@@ -33,7 +34,7 @@ void InitializeDisk(FILE *ms ,Disk D){
 
 // Mettre à jour tableau d'allocation
 
-void Metajour(FILE *ms, int idbloc){
+void Update_FAT(FILE *ms, int idbloc){
   // idbloc = le numero du bloc qui a été modifié
 bool v=true;
 fseek(ms, idbloc*sizeof(bool), SEEK_SET);
@@ -45,11 +46,13 @@ fwrite(&v, sizeof(bool), 1, ms);
 void InitializeBlock (Disk D,Block *B) { //initialiser les blocks
     B->student=malloc(sizeof(Student)*D.bf);
     B->num=0;
+    B->next=-1;
     //a loop to initialize the block so that all the records in it will have the same elements
     for (int i=0;i<D.bf;i++){
         B->student[i].group=0;
         B->student[i].ID=0;
         strcpy(B->student[i].name,"INITIALIZED !");
+        B->student[i].deleted=false;
     }
 }
 
@@ -77,10 +80,14 @@ void Display_Block(int Block_Number,FILE*ms,Disk D,Block * buffer) {
     rewind(ms);
     fseek(ms,sizeof(bool)*D.blocks,SEEK_SET);
     //this moves the cursor to the end of the file allocation table then we would start reading the blocks bellow it
-    fseek(ms,(sizeof(Student)*D.bf+sizeof(int))*Block_Number,SEEK_CUR);
+    fseek(ms,(sizeof(Student)*D.bf+(sizeof(int)*2))*Block_Number,SEEK_CUR);
     //this moves the cursor to the desired block
     fread(buffer->student,sizeof(Student),D.bf,ms);
-    fread(buffer->num,sizeof(int),1,ms);
+    printf("read students");
+    fread(&buffer->num,sizeof(int),1,ms);
+    printf("read the number");
+    fread(&buffer->next,sizeof(int),1,ms);
+    printf("read the next");
 }
 
 
--- util.h
+++ util.h
@@ -8,11 +8,13 @@ typedef struct Student {
 	char name[20];
 	int  ID;
 	int group;
+	bool deleted;
 }Student;
 
 typedef struct Block {
 	Student *student;
 	int num;
+	int next; //pointer to the next block it is going to be : -1 for contiguous files , other values for chained files
 }Block;
 
 typedef struct Disk {
