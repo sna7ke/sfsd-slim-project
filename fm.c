ded77fdc7e1af97328b5aed2de32c239c6f5a3df
rizou adem <ademrizou320@gmail.com>
2024-12-13 15:08:03 +0100

    ajoute de la fonction insert

--- fm.c
+++ fm.c
@@ -105,3 +105,212 @@ bool fileExists(FILE *ms, Disk D, const char* fName) {
     printf("The file does not exist.\n");
     return false;
 }
+void insertStudent(FILE *ms, Disk *D, Student newStudent, Meta *meta) {
+    Block buffer;
+    buffer.num = 0;
+    buffer.next = -1;
+    for (int i=0;i<D->bf;i++) {
+        buffer.student[i].ID = 0;
+        buffer.student[i].group = 0;
+        buffer.student[i].deleted = false;
+        strcpy(buffer.student[i].name, "");
+    }
+
+    if (meta->orgGlobal == 1) {
+        if (meta->orgInterne == 2) {
+            int i = meta->adress1stBlock;
+
+            while (i != -1) {
+                offset(ms, *D, i);
+                fread(&buffer, sizeof(Block), 1, ms);
+                if (buffer.num < D->bf) {
+                    buffer.student[buffer.num] = newStudent;
+                    buffer.student[buffer.num].deleted = false;
+                    buffer.num++;
+                    fseek(ms, -sizeof(Block), SEEK_CUR);
+                    fwrite(&buffer, sizeof(Block), 1, ms);
+
+                    meta->tailleEnRecord++;
+                    return;
+                }
+
+                i = buffer.next;
+            }//si ya pas de place
+            buffer.student[0] = newStudent;
+            buffer.student[0].deleted = false;
+            buffer.num = 1;
+            buffer.next = -1;
+
+            fseek(ms, 0, SEEK_END);
+            fwrite(&buffer, sizeof(Block), 1, ms);//le nv  bloc est ajoutee just apres le dernier blc
+            if (D->blocks > 0) {
+               fseek(ms, sizeof(bool)*D->blocks + (D->blocks - 1) * sizeof(Block)+sizeof(int), SEEK_SET);
+                int nvblcidc = D->blocks;
+                fwrite(&nvblcidc, sizeof(int), 1, ms);
+            }
+            meta->tailleEnRecord++;
+            D->blocks++;
+            meta->tailleEnBlock++;
+            return;
+        }
+        else if (meta->orgInterne == 1) {
+            int i = meta->adress1stBlock;
+
+            while (i != -1) {
+                offset(ms, *D, i);
+
+                fread(&buffer, sizeof(Block), 1, ms);
+                for (int j = 0; j < buffer.num; j++) {
+                    if (newStudent.ID < buffer.student[j].ID) {
+                        for (int k = buffer.num; k > j; k--) {
+                            buffer.student[k] = buffer.student[k-1];
+                        }
+                        buffer.student[j] = newStudent;
+                        buffer.student[j].deleted = false;
+                        buffer.num++;
+                        fseek(ms, -sizeof(Block), SEEK_CUR);
+                        fwrite(&buffer, sizeof(Block), 1, ms);
+                        meta->tailleEnRecord++;
+                        return;
+                    }
+                }
+                if (buffer.next == -1 || newStudent.ID < buffer.next) {
+                    if (buffer.num < D->bf) {
+                        buffer.student[buffer.num] = newStudent;
+                        buffer.student[buffer.num].deleted = false;
+                        buffer.num++;
+                        fseek(ms, -sizeof(Block), SEEK_CUR);
+                        fwrite(&buffer, sizeof(Block), 1, ms);
+
+                        meta->tailleEnRecord++;
+                        return;
+                    }
+                }
+                 // le prochain blc
+                i = buffer.next;
+            }
+
+               buffer.num = 0;
+        buffer.next = -1;
+        for (int i = 0; i < D->bf; i++) {
+        buffer.student[i].ID = 0;
+        buffer.student[i].group = 0;
+        buffer.student[i].deleted = false;
+        strcpy(buffer.student[i].name, "");
+    }
+            buffer.student[0] = newStudent;
+            buffer.student[0].deleted = false;
+            buffer.num = 1;
+            buffer.next = -1;
+
+            // ajt un nv blc
+            fseek(ms, 0, SEEK_END);
+            fwrite(&buffer, sizeof(Block), 1, ms);
+
+            // misaj  le pointeur du dernier bloc
+            if (D->blocks > 0) {
+               fseek(ms, sizeof(bool)*D->blocks + (D->blocks - 1) * sizeof(Block)+sizeof(int), SEEK_SET);
+                int nvblcidx = D->blocks;
+                fwrite(&nvblcidx, sizeof(int), 1, ms);
+            }
+
+            // misaj metadata
+            meta->tailleEnRecord++;
+            D->blocks++;
+            meta->tailleEnBlock++;
+            return;
+        }
+    }
+    else if (meta->orgGlobal == 2) {
+        if (meta->orgInterne == 2) {
+            for (int i = 0; i< D->blocks; i++) {
+                // Seek to the specific block
+                offset(ms, *D,i);
+                // lire le bloc
+                fread(&buffer, sizeof(Block), 1, ms);
+                 // verifier si le bloc a de lespace
+                if (buffer.num < D->bf) {
+                    buffer.student[buffer.num] = newStudent;
+                    buffer.student[buffer.num].deleted = false;
+                    buffer.num++;
+                    buffer.next = -1;
+                    fseek(ms, -sizeof(Block), SEEK_CUR);
+                    fwrite(&buffer, sizeof(Block), 1, ms);
+
+                    // misaj metadata
+                    meta->tailleEnRecord++;
+                    return;
+                }
+            }
+
+            // si toutes les blocs ne conitent pas despace en ajoute un nv blc
+            memset(&buffer, 0, sizeof(Block));
+            buffer.student[0] = newStudent;
+            buffer.student[0].deleted = false;
+            buffer.num = 1;
+            buffer.next = -1;
+
+            // Aajjouter un nv blc
+            fseek(ms, 0, SEEK_END);
+            fwrite(&buffer, sizeof(Block), 1, ms);
+
+            // misaj metadata
+            meta->tailleEnRecord++;
+            D->blocks++;
+            meta->tailleEnBlock++;
+            return;
+        }
+        else if (meta->orgInterne == 1) {
+            for (int i = 0; i< D->blocks; i++) {
+                offset(ms, *D, i);
+                fread(&buffer, sizeof(Block), 1, ms);
+               // trouverle point dinsertion dans les blocs
+                for (int j = 0; j < buffer.num; j++) {
+                    if (newStudent.ID < buffer.student[j].ID) {
+                        // Shift records to make space
+                        for (int k = buffer.num; k > j; k--) {
+                            buffer.student[k] = buffer.student[k-1];
+                        }
+
+                        // Inserer un nv etudiant
+                        buffer.student[j] = newStudent;
+                        buffer.student[j].deleted = false;
+                        buffer.num++;
+
+                        // Overwrite block
+                        fseek(ms, -sizeof(Block), SEEK_CUR);
+                        fwrite(&buffer, sizeof(Block), 1, ms);
+
+                        // misaj metadata
+                        meta->tailleEnRecord++;
+                        return;
+                    }
+                }
+            }
+
+            // Isiya pas despace en ajoute un nv
+           buffer.num = 0;
+    buffer.next = -1;
+    for (int i = 0; i < D->bf; i++) {
+        buffer.student[i].ID = 0;
+        buffer.student[i].group = 0;
+        buffer.student[i].deleted = false;
+        strcpy(buffer.student[i].name, "");
+    }
+            buffer.student[0] = newStudent;
+            buffer.student[0].deleted = false;
+            buffer.num = 1;
+            buffer.next = -1;
+
+            //ajouter un nv bloc
+            fseek(ms, 0, SEEK_END);
+            fwrite(&buffer, sizeof(Block), 1, ms);
+
+            // misajour metadata
+            meta->tailleEnRecord++;
+            D->blocks++;
+            meta->tailleEnBlock++;
+            return;
+        }
+    }
+}
--- fm.h
+++ fm.h
@@ -3,3 +3,4 @@
 Block fillBuffer(Disk d);
 void creatFile (FILE *ms, Disk *D);
 bool fileExists(FILE *ms, Disk D, const char* fName) ;
+insertStudent(FILE *ms, Disk *D, Student newStudent, Meta *meta);
